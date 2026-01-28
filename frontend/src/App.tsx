172314b191fda3bac1f975ec20eb2ef4c6029d99
Meresk <Yakovx95@mail.ru>
2026-01-29 01:45:03 +0300

    backend + frontend/ added start projects

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "@excalidraw/excalidraw/index.css";
+import { Excalidraw } from '@excalidraw/excalidraw'
+
+function App() {
+  return (
+    <>
+    <div style={{width: "100%", height: "100%"}}>
+      <Excalidraw />
+    </div>
+    </>
+  )
+}
+
+export default App
